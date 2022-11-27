44cbd2d19459ab3add60de3e441eda1bedbd20f2
TheHamsterHam <hu.yunlan1@gmail.com>
2022-11-27 00:24:20 -0500

    JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -5,7 +5,7 @@ const playerSelection = prompt("Do you choose rock, paper, or scissor?").toLocal
     if(playerSelection ==="paper" || playerSelection ==="rock" || playerSelection ==="scissor"){
         console.log("You chose " + `${playerSelection}`);
     } else {
-        console.log("Error! Please choice rock, paper, or scissor");
+        console.log("Error! Please choose rock, paper, or scissor");
     }
 
 
@@ -16,12 +16,20 @@ console.log("Computer chose" + ` ${getComputerChoice}`);
 //winner and loser
 const winner = declareWinner(playerSelection, getComputerChoice);
 
+//rock
 function declareWinner(playerSelection, getComputerChoice) {
-  if (playerSelection === 'rock' && getComputerChoice === 'paper') {
+if (playerSelection === 'rock' && getComputerChoice === 'paper') {
     console.log('You lose! Rock beats Paper');
-  } else if (playerSelection === 'scissor' && getComputerChoice === 'paper') {
+  } else if (playerSelection === 'rock' && getComputerChoice === 'scissor') {
     console.log('You win! Scissor beats Paper');
-  } else {
-    console.log('You tie!');
-  }
+  } else if (playerSelection === 'rock' && getComputerChoice === 'rock') {
+    console.log('It is a tie!');
+//paper
+    else if (playerSelection === 'paper' && getComputerChoice === 'rock') {
+    console.log('You win! paper beats rock');
+  } else if (playerSelection === 'paper' && getComputerChoice === 'scissor') {
+    console.log('You win! Scissor beats Paper');
+  } else if (playerSelection === 'paper' && getComputerChoice === 'paper') {
+    console.log('It is a tie!');
+
 }
