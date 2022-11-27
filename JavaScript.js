f4974f14fd70813bab4261278135a18d5b3b8bbc
TheHamsterHam <hu.yunlan1@gmail.com>
2022-11-27 15:44:57 -0500

    JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -14,22 +14,34 @@ const computerplay = () => {
 const playerRound = (playerSelection, ComputerSelection) => {
 if (playerSelection === 'rock' && ComputerSelection === 'paper') {
     console.log('You lose! Rock beats Paper');
+    //score for computer add one (+1)
+    compScore++
   } else if (playerSelection === 'rock' && ComputerSelection === 'scissor') {
     console.log('You win! Scissor beats Paper');
+    //score for player add one (+1)
+    playerScore++
   } else if (playerSelection === 'rock' && ComputerSelection === 'rock') {
     console.log('It is a tie!');
 //paper
   } else if (playerSelection === 'paper' && ComputerSelection === 'rock') {
     console.log('You win! Paper beats Rock');
+    //score for player add one (+1)
+    playerScore++
   } else if (playerSelection === 'paper' && ComputerSelection === 'scissor') {
     console.log('You lose! Scissor beats Paper');
+    //score for computer add one (+1)
+    compScore++
   } else if (playerSelection === 'paper' && ComputerSelection === 'paper') {
     console.log('It is a tie!');
 //scissor
   } else if (playerSelection === 'scissor' && ComputerSelection === 'rock') {
     console.log('You lose! Rock beats Scissor');
+    //score for computer add one (+1)
+    compScore++
   } else if (playerSelection === 'scissor' && ComputerSelection === 'paper') {
     console.log('You win! Scissor beats Paper');
+    //score for player add one (+1)
+    playerScore++
   } else if (playerSelection === 'scissor' && ComputerSelection === 'scissor') {
     console.log('It is a tie!');
   }
@@ -40,8 +52,16 @@ const playerSelection = 'rock'
 const game = () => {
   for (let i = 0; i < 5; i++) {
     const ComputerSelection = computerplay(); 
-    console.log(playerRound(playerSelection, ComputerSelection))
+    playerRound(playerSelection, ComputerSelection)
+  }
+
+  if (playerScore > compScore) {
+    return 'You have won the game!'
+  } else if (playerScore < compScore) {
+      return 'The computer won the game!'
+  } else {
+    return 'You tie!'
   }
 }
 
-game()
+console.log(game())
